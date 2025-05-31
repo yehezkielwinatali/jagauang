54a871f87292b2e2d8a44d5bc37be889ee1fb38b
yehezkielwinatali <yehezkielangg@gmail.com>
2025-06-01 01:57:50 +0900

    removing turbo

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbo: false,
-  },
-  images:{
-    remotePatterns:[
+  images: {
+    remotePatterns: [
       {
-        protocol:"https",
-        hostname:"randomuser.me"
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "randomuser.me",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
